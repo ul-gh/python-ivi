e700517990755bd6f88a7ee68848f0931927f2ba
Alex Forencich <alex@alexforencich.com>
2017-07-17 15:03:44 -0400

    Add Anritsu MN9610B attenuator

--- ivi/anritsu/__init__.py
+++ ivi/anritsu/__init__.py
@@ -2,9 +2,7 @@
 
 Python Interchangeable Virtual Instrument Library
 
-Copyright (c) 2012-2017 Alex Forencich
-
-Modified by Jeff Wurzbach 2014
+Copyright (c) 2017 Alex Forencich
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,36 +24,6 @@ THE SOFTWARE.
 
 """
 
-__all__ = [
-        # Base IVI class
-        "ivi",
-        # IVI abstract classes
-        "scope",
-        "dmm",
-        "fgen",
-        "dcpwr",
-        "swtch",
-        "pwrmeter",
-        "specan",
-        "rfsiggen",
-        "counter",
-        # Extra IVI base classes
-        "extra",
-        # Generic IVI drivers
-        "scpi",
-        # IVI drivers
-        "agilent",
-        "anritsu",
-        "dicon",
-        "chroma",
-        "colby",
-        "ics",
-        "jdsu",
-        "lecroy",
-        "rigol",
-        "tektronix",
-        "testequity"]
-
-from .ivi import *
-from . import *
+# Optical attenuators
+from .anritsuMN9610B import anritsuMN9610B
 
