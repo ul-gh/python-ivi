f2ccf34fedbc5191923a28c22032b5fc23255bf2
Alex Forencich <alex@alexforencich.com>
2017-08-14 15:17:23 -0400

    Add initial support for Tektronix MSO/DPO 5000, 7000, 70000 series scopes

--- ivi/tektronix/__init__.py
+++ ivi/tektronix/__init__.py
@@ -66,6 +66,78 @@ from .tektronixMDO3052 import tektronixMDO3052
 from .tektronixMDO3054 import tektronixMDO3054
 from .tektronixMDO3102 import tektronixMDO3102
 from .tektronixMDO3104 import tektronixMDO3104
+# DPO5000
+from .tektronixDPO5034 import tektronixDPO5034
+from .tektronixDPO5054 import tektronixDPO5054
+from .tektronixDPO5104 import tektronixDPO5104
+from .tektronixDPO5204 import tektronixDPO5204
+# DPO5000B
+from .tektronixDPO5034B import tektronixDPO5034B
+from .tektronixDPO5054B import tektronixDPO5054B
+from .tektronixDPO5104B import tektronixDPO5104B
+from .tektronixDPO5204B import tektronixDPO5204B
+# MSO5000
+from .tektronixMSO5034 import tektronixMSO5034
+from .tektronixMSO5054 import tektronixMSO5054
+from .tektronixMSO5104 import tektronixMSO5104
+from .tektronixMSO5204 import tektronixMSO5204
+# MSO5000B
+from .tektronixMSO5034B import tektronixMSO5034B
+from .tektronixMSO5054B import tektronixMSO5054B
+from .tektronixMSO5104B import tektronixMSO5104B
+from .tektronixMSO5204B import tektronixMSO5204B
+# DPO7000
+from .tektronixDPO7054 import tektronixDPO7054
+from .tektronixDPO7104 import tektronixDPO7104
+from .tektronixDPO7254 import tektronixDPO7254
+# DPO7000C
+from .tektronixDPO7054C import tektronixDPO7054C
+from .tektronixDPO7104C import tektronixDPO7104C
+from .tektronixDPO7254C import tektronixDPO7254C
+from .tektronixDPO7354C import tektronixDPO7354C
+# DPO70000
+from .tektronixDPO70404 import tektronixDPO70404
+from .tektronixDPO70604 import tektronixDPO70604
+from .tektronixDPO70804 import tektronixDPO70804
+from .tektronixDPO71254 import tektronixDPO71254
+from .tektronixDPO71604 import tektronixDPO71604
+from .tektronixDPO72004 import tektronixDPO72004
+# DPO70000B
+from .tektronixDPO70404B import tektronixDPO70404B
+from .tektronixDPO70604B import tektronixDPO70604B
+from .tektronixDPO70804B import tektronixDPO70804B
+from .tektronixDPO71254B import tektronixDPO71254B
+from .tektronixDPO71604B import tektronixDPO71604B
+from .tektronixDPO72004B import tektronixDPO72004B
+# DPO70000C
+from .tektronixDPO70404C import tektronixDPO70404C
+from .tektronixDPO70604C import tektronixDPO70604C
+from .tektronixDPO70804C import tektronixDPO70804C
+from .tektronixDPO71254C import tektronixDPO71254C
+from .tektronixDPO71604C import tektronixDPO71604C
+from .tektronixDPO72004C import tektronixDPO72004C
+# DPO70000DX
+from .tektronixDPO72304DX import tektronixDPO72304DX
+from .tektronixDPO72504DX import tektronixDPO72504DX
+from .tektronixDPO73304DX import tektronixDPO73304DX
+# MSO70000
+from .tektronixMSO70404 import tektronixMSO70404
+from .tektronixMSO70604 import tektronixMSO70604
+from .tektronixMSO70804 import tektronixMSO70804
+from .tektronixMSO71254 import tektronixMSO71254
+from .tektronixMSO71604 import tektronixMSO71604
+from .tektronixMSO72004 import tektronixMSO72004
+# MSO70000C
+from .tektronixMSO70404C import tektronixMSO70404C
+from .tektronixMSO70604C import tektronixMSO70604C
+from .tektronixMSO70804C import tektronixMSO70804C
+from .tektronixMSO71254C import tektronixMSO71254C
+from .tektronixMSO71604C import tektronixMSO71604C
+from .tektronixMSO72004C import tektronixMSO72004C
+# MSO70000DX
+from .tektronixMSO72304DX import tektronixMSO72304DX
+from .tektronixMSO72504DX import tektronixMSO72504DX
+from .tektronixMSO73304DX import tektronixMSO73304DX
 
 # Function Generators
 from .tektronixAWG2005 import tektronixAWG2005
